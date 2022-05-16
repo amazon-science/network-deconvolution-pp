3d796af525e489e4487e659cfb83a59142b4c626
Chengxi Ye <yechengxi@gmail.com>
2022-05-15 17:07:32 -0700

    Update deconv.py

--- Segmentation/models/segmentation/deconv.py
+++ Segmentation/models/segmentation/deconv.py
@@ -12,7 +12,10 @@ from torch.nn.parameter import Parameter
 from torch.nn import init
 
 import diffdist
+import numpy as np
+
 import cv2
+
 
 
 class Delinear(nn.Module):
@@ -130,16 +133,10 @@ class Delinear(nn.Module):
 class Deconv(conv._ConvNd):
 
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1,groups=1,bias=True, eps=1e-5, n_iter=5, momentum=0.1, block=64, sampling_stride=3,freeze=False,freeze_iter=100,sync=False,norm_type='none'):
-  
-        self.momentum = momentum
-        self.n_iter = n_iter
-        self.eps = eps
-        self.counter=0
         super(Deconv, self).__init__(
             in_channels, out_channels,  _pair(kernel_size), _pair(stride), _pair(padding), _pair(dilation),
             False, _pair(0), groups, bias, padding_mode='zeros')
-
-
+        
         if block > in_channels:
             block = in_channels
         else:
@@ -162,6 +159,10 @@ class Deconv(conv._ConvNd):
             self.register_buffer('running_mean', torch.zeros(kernel_size ** 2 * in_channels))
             self.register_buffer('running_cov_isqrt', torch.eye(self.num_features).repeat(in_channels // block, 1, 1))
 
+        self.momentum = momentum
+        self.n_iter = n_iter
+        self.eps = eps
+        self.counter=0
         self.sampling_stride=sampling_stride*stride
         self.counter=0
         self.freeze_iter=freeze_iter
@@ -212,15 +213,16 @@ class Deconv(conv._ConvNd):
                 X = X.view(-1, self.num_features, C // B).transpose(1, 2).contiguous().view(-1, self.num_features)
             else:
                 X=X.view(-1,X.shape[-1])
-
+                
             # 2. calculate mean,cov,cov_isqrt
             X_mean = X.mean(0)
 
+            M = X.shape[0]#number of pixels involved
+
             if self.groups==1:
-                M = X.shape[0]
                 XX_mean = X.t()@X/M 
             else:
-                M = X.shape[1]
+                X = X.view(-1, self.groups, self.num_features).transpose(0, 1)
                 XX_mean = X.transpose(1,2)@X/M 
                 
             if self.sync:
@@ -246,15 +248,17 @@ class Deconv(conv._ConvNd):
                 cov_isqrt = isqrt_newton_schulz_autograd(cov, self.n_iter)
             else:
                 Id = torch.eye(self.num_features, dtype=X.dtype, device=X.device).expand(self.groups, self.num_features, self.num_features)
-                cov= (XX_mean- (X_mean.unsqueeze(2)) @X_mean.unsqueeze(1))+self.eps*Id
+                X_mean_reshaped=X_mean.view(self.groups, self.num_features)                
+                cov= (XX_mean- (X_mean_reshaped.unsqueeze(2)) @X_mean_reshaped.unsqueeze(1))+self.eps*Id
                 cov_isqrt = isqrt_newton_schulz_autograd_batch(cov, self.n_iter)
+                
 
             # track stats for evaluation.
             self.running_mean.mul_(1 - self.momentum)                
             self.running_mean.add_(X_mean.detach() * self.momentum)
             self.running_cov_isqrt.mul_(1 - self.momentum)
             self.running_cov_isqrt.add_(cov_isqrt.detach() * self.momentum)
-
+            
         else:
             X_mean = self.running_mean
             cov_isqrt = self.running_cov_isqrt
@@ -273,6 +277,158 @@ class Deconv(conv._ConvNd):
         x= F.conv2d(x, w, b, self.stride, self.padding, self.dilation, self.groups)
 
         return x
+
+
+#class DeconvTransposed(conv._ConvTransposeNd): #latest pytorch
+class DeconvTransposed(conv._ConvTransposeMixin):#backward compatibility
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, groups=1,bias=True, dilation=1, eps=1e-5, n_iter=5, momentum=0.1, block=64, sampling_stride=3,sync=False,norm_type='none'):
+
+        self.momentum = momentum
+        self.n_iter = n_iter
+        self.eps = eps
+        self.counter=0
+        super(DeconvTransposed, self).__init__(
+            in_channels, out_channels, _pair(kernel_size), _pair(stride), _pair(padding), _pair(dilation),
+            True, _pair(output_padding), groups, bias, padding_mode='zeros')
+
+        if block > in_channels:
+            block = in_channels
+        else:
+            if in_channels%block!=0:
+                block=math.gcd(block,in_channels)
+
+        if groups>1:
+            #grouped conv
+            block=in_channels//groups
+
+        self.block=block
+
+        self.num_features = kernel_size**2 *block
+        if groups==1:
+            self.register_buffer('running_mean', torch.zeros(self.num_features))
+            self.register_buffer('running_cov_isqrt', torch.eye(self.num_features))
+        else:
+            self.register_buffer('running_mean', torch.zeros(kernel_size ** 2 * in_channels))
+            self.register_buffer('running_cov_isqrt', torch.eye(self.num_features).repeat(in_channels // block, 1, 1))
+
+        self.sampling_stride=sampling_stride*stride
+        self.counter=0
+        self.norm_type = norm_type
+        self.sync=sync
+        self.process_group=None
+
+        if self.norm_type=='layernorm':
+            self.layernorm=LayerNorm(self.eps)
+
+    def _specify_process_group(self, process_group):
+        self.process_group = process_group
+
+    def forward(self, x,output_size=None):
+        if x.numel()==0:
+            return x
+        N, C, H, W = x.shape
+        B = self.block
+        
+        x=x.contiguous()
+        if self.norm_type=='l1norm':
+            x_norm=x.abs().mean(dim=(1,2,3),keepdim=True)
+            x =  x/ (x_norm + self.eps)
+
+        elif self.norm_type=='layernorm':
+            x=self.layernorm(x)
+
+        if self.training:
+            self.counter+=1
+
+            #1. im2col: N x cols x pixels -> N*pixles x cols
+            if self.kernel_size[0]>1:
+                #the adjoint of a conv operation is a full correlation operation. So pad first.
+                padding=(self.padding[0]+self.kernel_size[0]-1,self.padding[1]+self.kernel_size[1]-1)
+                X = torch.nn.functional.unfold(x, self.kernel_size,self.dilation,self.padding,self.sampling_stride).transpose(1, 2).contiguous()
+            else:
+                #channel wise
+                X = x.permute(0, 2, 3, 1).contiguous().view(-1, C)[::self.sampling_stride**2,:]
+
+            if self.groups==1:
+                # (C//B*N*pixels,k*k*B)
+                X = X.view(-1, self.num_features, C // B).transpose(1, 2).contiguous().view(-1, self.num_features)
+            else:
+                X=X.view(-1,X.shape[-1])
+
+            # 2. calculate mean,cov,cov_isqrt
+
+            X_mean = X.mean(0)
+
+            if self.groups==1:
+                M = X.shape[0]
+                XX_mean = X.t()@X/M 
+            else:
+                M = X.shape[1]
+                XX_mean = X.transpose(1,2)@X/M 
+
+            if self.sync:
+                process_group = self.process_group
+                world_size = 1
+                if not self.process_group:
+                    process_group = torch.distributed.group.WORLD
+                if torch.distributed.is_initialized():
+                    world_size = torch.distributed.get_world_size(process_group)
+                    #sync once implementation:
+                    sync_data=torch.cat([X_mean.view(-1),XX_mean.view(-1)],dim=0)
+                    sync_data_list=[torch.empty_like(sync_data) for k in range(world_size)]
+                    sync_data_list = diffdist.functional.all_gather(sync_data_list, sync_data)
+                    sync_data=torch.stack(sync_data_list).mean(0)
+                    X_mean=sync_data[:X_mean.numel()].view(X_mean.shape)
+                    XX_mean=sync_data[X_mean.numel():].view(XX_mean.shape)
+                    
+
+            if self.groups==1:
+                Id = torch.eye(XX_mean.shape[1], dtype=X.dtype, device=X.device)
+                cov= XX_mean- X_mean.unsqueeze(1) @X_mean.unsqueeze(0)+self.eps*Id
+                cov_isqrt = isqrt_newton_schulz_autograd(cov, self.n_iter)
+            else:
+                Id = torch.eye(self.num_features, dtype=X.dtype, device=X.device).expand(self.groups, self.num_features, self.num_features)
+                cov= (XX_mean- (X_mean.unsqueeze(2)) @X_mean.unsqueeze(1))+self.eps*Id
+                cov_isqrt = isqrt_newton_schulz_autograd_batch(cov, self.n_iter)
+
+
+            self.running_mean.mul_(1 - self.momentum)
+            self.running_mean.add_(X_mean.detach() * self.momentum)
+            
+            self.running_cov_isqrt.mul_(1 - self.momentum)
+            self.running_cov_isqrt.add_(cov_isqrt.detach() * self.momentum)
+
+
+        else:
+            X_mean = self.running_mean
+            cov_isqrt = self.running_cov_isqrt
+
+        #3. S * X * D * W = (S * X) * (D * W)
+
+        # the difference between conv and corr is the flipped kernel
+        weight=torch.flip(self.weight,[2,3])
+
+        if self.groups==1:
+            w = weight.view(-1, self.num_features, C // B).transpose(1, 2).contiguous().view(-1,self.num_features) @ cov_isqrt
+            b = self.bias - (w @ (X_mean.unsqueeze(1))).view(self.weight.shape[0], -1).sum(1)
+            w = w.view(-1, C // B, self.num_features).transpose(1, 2).contiguous()
+        else:
+            w = self.weight.view(C//B, -1,self.num_features)@cov_isqrt
+            b = self.bias - (w @ (X_mean.view( -1,self.num_features,1))).view(self.bias.shape)
+
+        w = w.view(self.weight.shape)
+
+        w=torch.flip(w.view(weight.shape),[2,3])
+
+        output_padding = self._output_padding(x, output_size, self.stride, self.padding, self.kernel_size)
+        
+        x = F.conv_transpose2d(
+            x, w, b, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
+
+        return x
+
 
 
 
@@ -332,7 +488,6 @@ def isqrt_newton_schulz_autograd_batch(A, numIters,method='inverse_newton'):
     return A_isqrt
 
 
-
 class LayerNorm(nn.Module):
     def __init__(self, eps=1e-4):
         super(LayerNorm, self).__init__()
@@ -340,7 +495,10 @@ class LayerNorm(nn.Module):
 
     def forward(self,x):
         x_shape=x.shape
-        x=x.reshape(x_shape[0],-1)
+        if len(x_shape)<=3:#linear layers
+            x=x.reshape(-1, x_shape[-1])
+        else:#conv layers
+            x=x.reshape(x_shape[0],-1)
         mean = x.mean(-1,keepdim=True)
         std = x.std(-1,keepdim=True)+ self.eps
         #x = (x - mean) / std #disaster
